6e49b458139f59b1dcd2468902268a9efc4cfaa3
williambl <william@williambl.com>
2020-11-09 15:15:31 +0000

    Update app page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
-import './App.css';
+import "98.css"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <header className="App-header">
+            <div className="App">
+                <div className="title-bar">
+                    <div className="title-bar-text">Hey there</div>
+                    <div className="title-bar-controls">
+                        <button aria-label="Minimize"></button>
+                        <button aria-label="Maximize"></button>
+                        <button aria-label="Close"></button>
+                    </div>
+                </div>
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    This is our test! It uses <strong>React.js</strong>.
+                </p>
+            </div>
+        </header>
+    );
 }
 
 export default App;
