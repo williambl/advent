6e4f321eff3169fa36db348cd33d67a09aeaa2d5
williambl <william@williambl.com>
2020-11-09 19:16:35 +0000

    Add window component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import "98.css"
 import "./App.css"
+import Window from "./components/Window"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    withRouter
+} from 'react-router-dom';
 
 function App() {
     return (
-        <header className="App-header">
-            <div className="App">
-                <div className="title-bar">
-                    <div className="title-bar-text">Hey there</div>
-                    <div className="title-bar-controls">
-                        <button aria-label="Minimize"></button>
-                        <button aria-label="Maximize"></button>
-                        <button aria-label="Close"></button>
-                    </div>
-                </div>
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    This is our test! It uses <strong>React.js</strong>.
-                </p>
-            </div>
-        </header>
+        <Router>
+            <Switch>
+                <Route>
+                    <Window width={300} height={260} title={"Test!!"}>
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <p>
+                            This is our test! It uses <strong>React.js</strong>.
+                        </p>
+                    </Window>
+                    <Window width={300} height={260} title={"Test 2!!"}>
+                        <img src={logo} className="App-logo" alt="logo" />
+                        <p>
+                            This is our test! It uses <strong>React.js</strong>.
+                        </p>
+                    </Window>
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
---
+++ src/components/Window.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Draggable from "react-draggable";
+
+export default class Window extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            width: props.width || "100%",
+            height: props.height || "100%",
+            onMinimise: props.onMinimise || function(){},
+            onMaximise: props.onMaximise || function(){},
+            onClose: props.onClose || function(){},
+            draggable: props.draggable === true,
+            children: props.children,
+            title: props.title || ""
+        }
+    }
+
+    render() {
+        return (
+            <Draggable handle=".title-bar">
+                <div style={{ width: this.state.width, height: this.state.height }} className="window">
+                    <div className="title-bar">
+                        <div className="title-bar-text">{this.state.title}</div>
+                        <div className="title-bar-controls">
+                            <button aria-label="Minimize" onClick={this.state.onMinimise}/>
+                            <button aria-label="Maximize" onClick={this.state.onMaximise}/>
+                            <button aria-label="Close" onClick={this.state.onClose}/>
+                        </div>
+                    </div>
+
+                    <div className="window-body">
+                        { this.state.children }
+                    </div>
+                </div>
+            </Draggable>
+
+        )
+    }
+}
