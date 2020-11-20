28fbf4b13af90a2007e58232c723f70484e5a82a
williambl <william@williambl.com>
2020-11-20 20:37:48 +0000

    Improve calendar

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,23 +1,38 @@
 import React, { Component } from "react";
 import Window from "../components/Window";
 
+const challengeDescriptions = new Array(25).fill([])
+
+const challengeImages = new Array(25).fill("https://cdn.discordapp.com/avatars/252528045330661378/a47168a6a60776d30e84d88ea3c60906.png")
+
+//temporary until i've written them all
+challengeDescriptions[0] = ["Just log in!"]
+challengeDescriptions[1] = ["bleh."]
+challengeDescriptions[2] = ["teeeeeest"]
+challengeDescriptions[3] = ["i cba to write descriptions for the rest"]
+
 export default class Calendar extends Component {
     render() {
-        const windowList = []
+        const entries = []
         for (let y=0; y <= 4; y++) for (let x=1; x <= 5; x++) {
             const id = y*5 + x
-            windowList.push(
-                <Window title={id} height={"8em"} width={"20%"} draggable={false} style={{flex: "1 0 auto"}}>
-                    <p>Challenge {id}</p>
-                    <button id={"open"+id}>Open</button>
-                </Window>
+            entries.push(
+                <fieldset style={{minHeight: "3em", width: "20%", flex: "1 0 auto", marginBottom: "10px", display: "flex", alignItems: "center"}}>
+                    <legend>Challenge #{id}</legend>
+                    <ul className="tree-view" style={{margin: "10px", width: "60%", height: "100%", flex: "0 1 auto"}}>
+                        {challengeDescriptions[id-1].map((it) => <li>{it}</li>)}
+                    </ul>
+                    <button style={{flex: "0 1 auto", width: "80px", height: "80px", backgroundImage: "URL(\""+challengeImages[id-1]+"\")", backgroundSize: "cover"}} />
+                </fieldset>
             )
         }
 
         return (
-            <div style={{"flex-wrap": "wrap", display: "flex" }}>
-                {windowList}
-            </div>
+            <Window draggable={false} title="Advent Calendar" style={{width: "auto"}}>
+                <div style={{"flex-wrap": "wrap", display: "flex" }}>
+                    {entries}
+                </div>
+            </Window>
         )
     }
-}
+                }
