5f88ed4cf60ad5982ebd04b71886a240cb89f758
williambl <william@williambl.com>
2020-11-20 18:47:06 +0000

    Add user avatar to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,26 @@ import { Link } from 'react-router-dom';
 import { Auth } from './Oauth';
 
 export default function Header (props) {
-        return (
-            <header className="window" style={{width: "100%", height: "70px"}}>
-                <Link to="/">
-                    <div className="title-bar">
-                        <div className="title-bar-text">Advent challenge 2020</div>
-                    </div>
-                </Link>
-                <div className="window-body" style={{textAlign: "right"}}>
-                    {
-                        props.isLoggedIn ?
-                            <Auth userInfo={props.userInfo} isLoggedIn={props.isLoggedIn} connectFunc={props.connectFunc} />
-                            :
-                            <p>Currently logged in as {props.userInfo.username}</p>
-                    }
+    return (
+        <header className="window" style={{width: "100%", height: "70px"}}>
+            <Link to="/">
+                <div className="title-bar">
+                    <div className="title-bar-text">Advent challenge 2020</div>
                 </div>
-            </header>
-        )
-    }
+            </Link>
+            <div className="window-body" style={{display: "flex", flexDirection: "row-reverse", alignItems: "center"}}>
+                {
+                    props.isLoggedIn ?
+                        <img className="inset" src={"https://cdn.discordapp.com/avatars"+props.userInfo.id+props.userInfo.avatar} style={{height: "40px", width: "40px"}} />
+                        :
+                        <div className="inset" style={{height: "40px", width: "40px"}} />
+                }
+                {
+                    <Auth userInfo={props.userInfo} isLoggedIn={props.isLoggedIn} connectFunc={props.connectFunc} />
+                }
+            </div>
+        </header>
+    )
+}
 
 
