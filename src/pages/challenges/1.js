ebb7d1da7c8cb1c604c683ff3150755452956945
williambl <william@williambl.com>
2020-11-27 20:32:42 +0000

    Add challenge #1

--- src/pages/challenges/1.js
+++ src/pages/challenges/1.js
@@ -4,20 +4,20 @@ import { answerChallenge, challengeStata } from '../../utils'
 import Window from '../../components/Window'
 
 export default function Challenge1(props) {
-    <AuthProvider><Challenge1Inner /></AuthProvider>
+    return (<AuthProvider><Challenge1Inner /></AuthProvider>)
 }
 
 function Challenge1Inner(props) {
-    if (props.isLoggedIn && !challengeStata[0]) answerChallenge(0, 0)
+    if (props.isLoggedIn && !challengeStata[0])
+        answerChallenge(0, 0)
 
     return (
-        <Window width={600} height={300} title={"Challenge #1" + challengeStata[0]}>
+        <Window width={300} height={150} title={"Challenge #1" + challengeStata[0] ? " Complete!" : ""}>
             <p>
                 Your first challenge is pretty easy: just click the log in button.
             </p>
 
-            <label>{props.isLoggedIn ? "Successfully logged in!" : "Log in"}</label>
+            <button>{props.isLoggedIn ? "Successfully logged in!" : "Log in"}</button>
         </Window>
-
     )
 }
