c1f9a746067f285cd07ed63dbf87e27de089d221
williambl <william@williambl.com>
2020-12-01 18:20:02 +0000

    Turn window into a functional component

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,41 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Draggable from "react-draggable";
 
-export default class Window extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            width: props.width || "100%",
-            height: props.height || "100%",
-            onMinimise: props.onMinimise || function(){},
-            onMaximise: props.onMaximise || function(){},
-            onClose: props.onClose || function(){},
-            draggable: props.draggable === undefined ? true : props.draggable,
-            children: props.children,
-            title: props.title || "",
-            style: props.style || ""
-        }
+export default function Window(props) {
+
+    const settings = {
+        width: props.width || "100%",
+        height: props.height || "100%",
+        onMinimise: props.onMinimise || function(){},
+        onMaximise: props.onMaximise || function(){},
+        onClose: props.onClose || function(){},
+        draggable: props.draggable === undefined ? true : props.draggable,
+        title: props.title || "",
+        style: props.style || ""
     }
-
-    render() {
-        return (
-            <Draggable handle=".title-bar" disabled={!this.state.draggable}>
-                <div style={Object.assign({ width: this.state.width, height: this.state.height }, this.state.style)} className="window">
-                    <div className="title-bar">
-                        <div className="title-bar-text">{this.state.title}</div>
-                        <div className="title-bar-controls">
-                            <button aria-label="Minimize" onClick={this.state.onMinimise}/>
-                            <button aria-label="Maximize" onClick={this.state.onMaximise}/>
-                            <button aria-label="Close" onClick={this.state.onClose}/>
-                        </div>
-                    </div>
-
-                    <div className="window-body">
-                        { this.state.children }
+    return (
+        <Draggable handle=".title-bar" disabled={!settings.draggable}>
+            <div style={Object.assign({ width: settings.width, height: settings.height }, settings.style)} className="window">
+                <div className="title-bar">
+                    <div className="title-bar-text">{settings.title}</div>
+                    <div className="title-bar-controls">
+                        <button aria-label="Minimize" onClick={settings.onMinimise}/>
+                        <button aria-label="Maximize" onClick={settings.onMaximise}/>
+                        <button aria-label="Close" onClick={settings.onClose}/>
                     </div>
                 </div>
-            </Draggable>
 
-        )
-    }
+                <div className="window-body">
+                    { props.children }
+                </div>
+            </div>
+        </Draggable>
+
+    )
 }
